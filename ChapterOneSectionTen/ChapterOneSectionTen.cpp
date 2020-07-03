e849760ed94247fdab07e0765cceed39a3948fa7
RhysSharpe <rhysgsharpe@gmail.com>
2020-07-03 23:38:20 +0100

    Task Completed
    Chapter 1, section 10.
    Completed the tasks of getting user input and manipulating it.

--- ChapterOneSectionTen/ChapterOneSectionTen.cpp
+++ ChapterOneSectionTen/ChapterOneSectionTen.cpp
@@ -1,7 +1,15 @@
 #include "pch.h"
 #include <iostream>
 
+// My solution to the challenge (didn't need changes after following tutorial)
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Enter a number:\n";
+
+    int input{ 0 };
+    std::cin >> input;
+
+    std::cout << "Your number is: " << input << '\n';
+    std::cout << "Your number doubled is: " << input * 2 << '\n';
+    std::cout << "Your number tripled is: " << input * 3 << '\n';
 }
