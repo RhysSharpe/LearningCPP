6ab9f0f0ae4cacd0252a8d65063f724b7fe64e0c
RhysSharpe <rhysgsharpe@gmail.com>
2020-07-04 23:12:20 +0100

    Chapter 2
    Chapter 2, section 1.
    Setup new project for chapter 2 and looked at functions.

---
+++ Chapter2/Chapter2.cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+#include <iostream>
+
+// Definition of user-defined function doPrint()
+void doPrint() // doPrint() is the called function in this example
+{
+    std::cout << "In doPrint()\n";
+}
+
+void doB()
+{
+    std::cout << "In doB()\n";
+}
+
+void doA()
+{
+    std::cout << "Starting doA()\n";
+    doB();
+    std::cout << "Ending doA()\n";
+}
+
+// Definition of function main()
+int main()
+{
+    std::cout << "Starting main()\n";
+
+    doPrint(); // Interrupt main() by making a function call to doPrint().  main() is the caller.
+    doPrint(); // doPrint() called for the second time
+
+    doA();
+
+    std::cout << "Ending main()\n"; // this statement is executed after doPrint() ends
+
+    return 0;
+}
