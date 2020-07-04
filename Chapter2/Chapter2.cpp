d228b500a3acd9d250c765db4009ddbbb58bb60d
RhysSharpe <rhysgsharpe@gmail.com>
2020-07-04 23:38:29 +0100

    Function Return Values
    Chapter 2, section 2.
    Looked at function return values.

--- Chapter2/Chapter2.cpp
+++ Chapter2/Chapter2.cpp
@@ -19,6 +19,25 @@ void doA()
     std::cout << "Ending doA()\n";
 }
 
+// int is the return type
+// A return type of int means the function will return some integer value to the caller (the specific value is not specified here)
+int returnFive()
+{
+    // the return statement indicates the specific value that will be returned
+    return 5; // return the specific value 5 back to the caller
+}
+
+// Function asks user to enter a value
+// Return value is the integer entered by the user from the keyboard
+int getValueFromUser() // this function now returns an integer value
+{
+    std::cout << "Enter an integer: ";
+    int input{};
+    std::cin >> input;
+
+    return input; // return the value the user entered back to the caller
+}
+
 // Definition of function main()
 int main()
 {
@@ -27,9 +46,16 @@ int main()
     doPrint(); // Interrupt main() by making a function call to doPrint().  main() is the caller.
     doPrint(); // doPrint() called for the second time
 
+    std::cout << "Ending main()\n\n"; // this statement is executed after doPrint() ends
+
     doA();
 
-    std::cout << "Ending main()\n"; // this statement is executed after doPrint() ends
+    std::cout << '\n' << returnFive() << '\n'; // prints 5
+    std::cout << returnFive() + 3 << '\n'; // prints 8
 
-    return 0;
+    int num{ getValueFromUser() }; // initialize num with the return value of getValueFromUser()
+    std::cout << num << " doubled is: " << num * 2 << '\n';
+
+    // Your main function should return 0 (EXIT_SUCCESS) if the program ran normally.
+    return EXIT_SUCCESS;
 }
